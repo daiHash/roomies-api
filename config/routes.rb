f093de42477787f6c3312f58ed76b9cd05cfbce4
Daichi Hashimura <dhashi92@gmail.com>
2017-12-17 18:19:42 +0900

    Generate user controller, Create register method and Add route path for register

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    post 'auth/register', to: 'users#register'
+  end  
 end
