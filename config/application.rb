f186daadd6f47a538f88a531746c89a6b9dcc1fb
Daichi Hashimura <dhashi92@gmail.com>
2018-01-07 04:09:17 +0900

    Configure rack-cors settings

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,9 @@ module RoomiesApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
     #autoloads lib folder during production
